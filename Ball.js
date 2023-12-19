b40dbc9714532235fc44a65a1c6ee1ca2cef72dc
calundrigan <lundrigancameron9@gmail.com>
2023-12-19 02:36:53 -0330

    update

--- Ball.js
+++ Ball.js
@@ -2,7 +2,27 @@ export default class Ball{
     constructor(ballElem){
         this.ballElem=ballElem
     }
+
+    get x(){
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    }
+
+    setX(value){
+        this.ballElem.style.setProperty("--x",value)
+    }
+
+
+    get y(){
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+    }
+
+    setY(value){
+        this.ballElem.style.setProperty("--y",value)
+    }
+
     update(delta){
-        
+        this.x = 5
+        this.y = 15
+
     }
 }
